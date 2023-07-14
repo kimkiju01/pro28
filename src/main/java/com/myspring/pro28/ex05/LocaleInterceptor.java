b52d242efcb75575c623be8f304fddaf7dd7f4e1
kimkiju01 <kimkiju01@gmail.com>
2023-07-14 19:53:13 +0900

    Initial commit

---
+++ src/main/java/com/myspring/pro28/ex05/LocaleInterceptor.java
@@ -0,0 +1,39 @@
+package com.myspring.pro28.ex05;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+public class LocaleInterceptor extends  HandlerInterceptorAdapter{
+	
+	   @Override
+	   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
+	      HttpSession session=request.getSession();
+	      String locale=request.getParameter("locale");
+	      if(locale==null)
+	         locale="en";
+	      session.setAttribute("org.springframework.web.servlet.i18n.SessionLocaleResolver.LOCALE",new Locale(locale));
+	      System.out.println("prehandle");
+	      return true;
+	   }
+
+	   @Override
+	   public void postHandle(HttpServletRequest request, HttpServletResponse response,
+	                           Object handler, ModelAndView modelAndView) throws Exception {
+		   
+		   System.out.println("posthandle");
+	   }
+
+	   @Override
+	   public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+	                                    Object handler, Exception ex)    throws  Exception {
+		   System.out.println("after");
+	   }
+	}
